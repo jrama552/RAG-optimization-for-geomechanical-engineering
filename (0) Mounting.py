c184d1952fa206fced04807623c80e1ff979a623
jrama552 <324185225+jrama552@users.noreply.github.com>
2026-09-17 14:15:37 -0700

    Update (0) Mounting.py

--- (0) Mounting.py
+++ (0) Mounting.py
@@ -1,18 +1,16 @@
-from google.colab import drive
 import json
 import re
+import os
 
-drive.mount('/content/drive')
-
-json_path = '/content/drive/MyDrive/file.json'
+json_path = 'data/synthetic_prompts.json'
 with open(json_path, 'r') as f:
-    data = json.load(f) 
+    data = json.load(f)
 
-true_code = [json.dumps(d['files']) for d in data]
-true_code = true_code[120:]
+synthetic_code = [json.dumps(d['files']) for d in data]
+synthetic_code = synthetic_code[120:]
 
-with open("drive/MyDrive/LLM project/file", "r") as file:
+with open("data/base_examples.json", "r") as file:
     base_code = json.load(file)
 
-with open("drive/MyDrive/LLM project/code/file", "r") as file:
+with open("data/few_shot_examples.json", "r") as file:
     few_shot_code = json.load(file)
