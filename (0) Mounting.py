4903e39b5b501580d354dfdf39500d17c6599ef5
jrama552 <324185225+jrama552@users.noreply.github.com>
2026-09-17 14:01:01 -0700

    Update (0) Mounting.py

--- (0) Mounting.py
+++ (0) Mounting.py
@@ -4,15 +4,15 @@ import re
 
 drive.mount('/content/drive')
 
-json_path = '/content/drive/MyDrive/LLM project/code/fei_project_prompts (3).json'
+json_path = '/content/drive/MyDrive/file.json'
 with open(json_path, 'r') as f:
     data = json.load(f) 
 
 true_code = [json.dumps(d['files']) for d in data]
 true_code = true_code[120:]
 
-with open("drive/MyDrive/LLM project/code/base_code(7).json", "r") as file:
+with open("drive/MyDrive/LLM project/file", "r") as file:
     base_code = json.load(file)
 
-with open("drive/MyDrive/LLM project/code/few_shot_code (5).json", "r") as file:
+with open("drive/MyDrive/LLM project/code/file", "r") as file:
     few_shot_code = json.load(file)
